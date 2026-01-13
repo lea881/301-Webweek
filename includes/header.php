eebb92795b348233c97a90165f624205049747fc
maelleeee1234 <maelle.bidet@etu.uca.fr>
2026-01-13 17:22:58 +0100

    header, ajout logo, navigation, css header

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,33 @@
 
 ?>
 
-<!--FOOTER-->
-<header>
+<!--HEADER-->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+
+<header class="header">
+  <div class="header-left">
+    <img src="../img/logo.jpg" alt="Logo" class="logo">
+  </div>
+
+  <nav class="menu">
+    <a href="stages.php">Stage</a>
+    <a href="cours.php">Cours</a>
+    <a href="association.php">Association</a>
+    <a href="histoire.php">Histoire</a>
+  </nav>
+
+  <div class="header-droite">
+    <span class="phone">06-10-90-24-32</span>
+    <a href="contact.html" class="contact">Contact</a>
+  </div>
 </header>
+
+</body>
+</html>
