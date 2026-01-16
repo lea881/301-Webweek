c3964455298538ac454df7d7aaac1ea33d6e94ec
Maxxiaa <maiaas.13005@gmail.com>
2026-01-16 14:59:04 +0100

    ajout de photos dans la galerie et des photos pour un carroussel

--- pages/association.php
+++ pages/association.php
@@ -19,18 +19,41 @@
     Aujourd’hui professeur au club d’Aïkido du Puy-en-Velay, il anime régulièrement des stages dans d’autres clubs auvergnats (Saint-Flour, CUC de Clermont-Ferrand…). Depuis 2013, il est également l’assistant d’Alain Royer (7ᵉ dan), directeur technique régional de la Ligue Auvergne, avec qui il coanime les stages de ligue.<br>
     Membre du collège technique national de la FFAAA de 2016 à 2019, Thomas Gavory est aussi formateur BF pour la région Auvergne–Rhône-Alpes.
 </p>
+
 <img src="../img/Thomas_Gavory.png" alt="photo de Thomas Gavory">
+
 <h1>Galerie</h1>
-<div class="galerie.photoscouleur">
-    <img src="../img/photo_cours.png" alt="photo de cours">
-    <img src="../img/photo-prise.png" alt="photo prise aikido">
-    <img src="../img/photo-sincline.png" alt="photo de thomas gavory et un de ses élève qui se prosterne">
-    <img src="../img/photo-prise-sol.png" alt="photo d'une prise au sol">
-    <img src="../img/photo-prise-tournoi.png" alt ="photo d'une pris d'aikido à un tournoi">
-    <img src="../img/photo-cours-tokyo.png" alt="photo d'un cours à tokyo">
-    <img src="../img/photo_cours-endo-sensei.png" alt="photo avec endo sensei">
-    <img src="../img/photo-groupe_1.png" alt="photo d'un groupe avec Thomas Gavory">
-    <img src="../img/photo-groupe_2.png" alt="photo de groupe avec Thomas Gavory">
-</div>
+    <div class="galerie.photoscouleur">
+        <img src="../img/photo_cours.png" alt="photo de cours">
+        <img src="../img/photo-prise.png" alt="photo prise aikido">
+        <img src="../img/photo-sincline.png" alt="photo de thomas gavory et un de ses élève qui se prosterne">
+        <img src="../img/photo-prise-sol.png" alt="photo d'une prise au sol">
+        <img src="../img/photo-prise-tournoi.png" alt ="photo d'une pris d'aikido à un tournoi">
+        <img src="../img/photo-cours-tokyo.png" alt="photo d'un cours à tokyo">
+        <img src="../img/photo_cours-endo-sensei.png" alt="photo avec endo sensei">
+        <img src="../img/photo-groupe_1.png" alt="photo d'un groupe avec Thomas Gavory">
+        <img src="../img/photo-groupe_2.png" alt="photo de groupe avec Thomas Gavory">
+    </div>
+
+    <div class="carousel.photos">
+        <div class="carousel-slides">
+            <div class="slide_active">
+                <img src="../img/photo-noir-blanc-couteau.png" alt="photo proche d'un combt avec couteau">
+            </div>
+            <div class="slide">
+                <img src="../img/photo-noir-blanc-prise.png" alt="prise d aikido">
+            </div>
+            <div class="slide">
+                <img src="../img/photo-noir-blanc_prise-proche.png" alt="prise vu de proche d aikido">
+            </div>
+            <div class="slide">
+                <img src="../img/photo-noir-blanc-main.png" alt="main vu de proche">
+            </div>
+        </div>
+
+        <button class="prev" id="prevBtn">&#10094;</button>
+        <button class="next" id="nextBtn">&#10095;</button>
+
+    </div>
 </body>
 </html>
