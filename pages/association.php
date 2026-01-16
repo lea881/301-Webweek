c46bcb85dc868999c47916d99baf9ad5a62adfaf
Maxxiaa <maiaas.13005@gmail.com>
2026-01-16 11:00:07 +0100

    changement html association

--- pages/association.php
+++ pages/association.php
@@ -6,18 +6,6 @@
     <title>Association</title>
 </head>
 <body>
-    <h1> Thomas Gavory </h1>
-    <h2> 6ème DAN</h2>
-    <p> Diplômé d'État </p>
-    <p>Thomas Gavory participe à de nombreux événements majeurs, notamment au Festival des Arts Martiaux de Bercy, où il intervient comme uke auprès de figures reconnues de l’aïkido : Christian Tissier Shihan (2001, 2003, 2004, 2007), Marc Bachraty Senseï (2011) et Yoko Okamoto Senseï (2012).
-En 2012, il se rend au Japon avec la délégation de la FFAAA et assiste Micheline Tissier Senseï lors du 11ᵉ Congrès de la Fédération Internationale d’Aïkido. <br>
-Après avoir enseigné à Noisy-le-Grand de 1994 à 2010 aux côtés de Pierre Helley, Thomas Gavory quitte la région parisienne pour s’installer au Puy-en-Velay. Il y ouvre le Dojo Mugamae, un lieu consacré pendant plusieurs années à la pratique et à la transmission de l’aïkido. <br> Depuis sa fermeture en 2020, les cours se poursuivent au dojo de la halle multisports Célestin Quincieu, à Aiguilhe.
-Aujourd’hui professeur au club d’Aïkido du Puy-en-Velay, il anime régulièrement des stages dans d’autres clubs auvergnats (Saint-Flour, CUC de Clermont-Ferrand…).<br> Depuis 2013, il est également l’assistant d’Alain Royer (7ᵉ dan), directeur technique régional de la Ligue Auvergne, avec qui il coanime les stages de ligue.
-Membre du collège technique national de la FFAAA de 2016 à 2019, Thomas Gavory est aussi formateur BF pour la région Auvergne–Rhône-Alpes.
-<h2>Galerie</h2>
-<div class="galerie">
-    <img src 
 
-</div>
 </body>
 </html>
