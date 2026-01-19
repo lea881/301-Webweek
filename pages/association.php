1aac838ccd44a3fc9f22709fa32948b8f1448df3
AgatheMarconnet <agathe.marconnet@etu.uca.fr>
2026-01-19 11:47:39 +0100

    correction header page cours

--- pages/association.php
+++ pages/association.php
@@ -10,7 +10,7 @@
     <?php
     //J'utilise la variable path pour que le chemin s'adapte en fonction de la page (ce ne sont pas les même chemin si on vient de index ou d'une autre page)
     $path = "../";
-     include '../includes/header.php'; 
+    include '../includes/header.php'; 
     ?>
    <h1>Thomas Gavory</h1>
    <h2> 6ème DAN</h2>
