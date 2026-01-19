2ad3028b2eedce528e784d3d96f8f9b1749bc683
Maxxiaa <maiaas.13005@gmail.com>
2026-01-19 11:15:34 +0100

    Merge branch 'main' of https://github.com/AgatheMarconnet/301-Webweek

--- includes/footer.php
+++ includes/footer.php
@@ -7,14 +7,14 @@
   <nav class="footer-menu">
     <ul>
       <li><a href="../pages/association.php">Association</a></li>
-      <li><a href="../pages/cours.php"><p>Cours</a></li>
-      <li><a href="../pages/histoire.php"><p>Histoire</a></li>
+      <li><a href="../pages/cours.php">Cours</a></li>
+      <li><a href="../pages/histoire.php">Histoire</a></li>
       <li><a href="../pages/stagecarte.php">Stage</a></li>
     </ul>
   </nav>
   <div class="footer-droite">
     <div class="footer-address">
-      <p><a href="https://www.google.com/maps/place/1+Av.+de+Bonneville,+43000+Aiguilhe/@45.0491074,3.8781186,504m/data=!3m2!1e3!4b1!4m6!3m5!1s0x47f5fb01d0ccb575:0xb0cb4d2a711f4204!8m2!3d45.0491326!4d3.8806173!16s%2Fg%2F11b8v4gj5d?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoASAFQAw%3D%3D"></a>1, avenue de Bonneville, 43000 Aiguilhe</p>
+      <a href="https://www.google.com/maps/place/1+Av.+de+Bonneville,+43000+Aiguilhe/@45.0491074,3.8781186,504m/data=!3m2!1e3!4b1!4m6!3m5!1s0x47f5fb01d0ccb575:0xb0cb4d2a711f4204!8m2!3d45.0491326!4d3.8806173!16s%2Fg%2F11b8v4gj5d?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoASAFQAw%3D%3D">1, avenue de Bonneville, 43000 Aiguilhe</a>
     </div>
     <div class="footer-rs">
         <a href="https://www.facebook.com/people/A%C3%AFkido-Le-Puy-en-Velay/100070297455766/" target="_blank">
